c8d68551fcb07e8cbbd7b8db722d4768dd8b33cc
86155 <1059330084@qq.com>
2020-07-12 11:31:50 +0800

    修复feign和hystrix工程运行失败，是因为spring cloud和springboot版本不对应导致，另外由于yml文件的编码格式为GBK，而工程的为utf-8，也会导致运行出错

--- qds-cloud-consumer-feign/src/test/java/com/gree/qds/qdscloudconsumerfeign/QdsCloudConsumerFeignApplicationTests.java
+++ qds-cloud-consumer-feign/src/test/java/com/gree/qds/qdscloudconsumerfeign/QdsCloudConsumerFeignApplicationTests.java
@@ -1,6 +1,6 @@
 package com.gree.qds.qdscloudconsumerfeign;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
